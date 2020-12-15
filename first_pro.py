cadb67ee62efadb526bb011078d3f9add0357af1
Ozodchik <dilovardehkanov730@gmail.com>
2020-12-15 18:37:41 +0300

    origin

--- first_pro.py
+++ first_pro.py
@@ -1,25 +1,26 @@
 import requests
 from urllib.parse import urljoin
 
-
 class Iterable:
+   def __init__(self, url):
+        get_response = requests.get(url)
+        self.lines = get_response.json()
+        self.cursor = 0
 
-    def __init__(self, url):
-        self.get_response = requests.get(url)
-        self.response_json = self.get_response.json()
+   def __iter__(self):
+      return self
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        raise StopIteration
-
-
+   def __next__(self):
+      if self.cursor == len(self.lines):
+         raise StopIteration
+      number = self.lines[self.cursor]
+      self.cursor += 1
+      names_countrys = number["name"]["common"].replace(" ", "_")
+      links = urljoin("https://en.wikipedia.org/wiki/", names_countrys)
+      return f'{names_countrys} - {links}'
 
 if __name__ == "__main__":
     with open("file.txt", "w", encoding="UTF-8") as f:
-        a = Iterable("https://raw.githubusercontent.com/mledoze/countries/master/countries.json")
-        for names in a.response_json:
-            country = names["name"]["common"].replace(" ", "_")
-            urls = urljoin("https://en.wikipedia.org/wiki/", country)
-            f.write(f'{country}-{urls} \n')
+        res = Iterable("https://raw.githubusercontent.com/mledoze/countries/master/countries.json")
+        for name in res:
+            f.write(f'{name} \n')
